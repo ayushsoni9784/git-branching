8cf866e3859485e540aed859e256697313bc3627
ayushsoni9784 <ayushsoni626@gmail.com>
2024-08-12 02:41:37 +0530

    app.py change

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 print("Hello Branching")
 print("New branch feature")
 print("New branch feature-1")
-<<<<<<< HEAD
 print("Added from main")
-=======
-print("Added from Feature-1")
->>>>>>> origin/feature-1
